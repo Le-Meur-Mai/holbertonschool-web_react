1f1b0d68eb11074b436a2fb4bb33d60c2015cfd9
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-15 18:57:13 +0100

    Creating an interface for a class and a constructor and using them to create the actual class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,14 +15,49 @@ interface Directors extends Teacher {
 
 // On etend l'interface de teacher et on rajoute un attribut
 
-interface printTeacherFunction {
+interface PrintTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 };
 
 // console.log(printTeacher("John", "Doe")); -> La commande de test
 
 // On cree une interface pour la fonction printTeacher
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentConstructorInterface {
+    firstName: string;
+    lastName: string;
+}
+
+// On prepare les interfaces pour la classe
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructorInterface) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+    
+}
+
+// On cree la class studentclass
