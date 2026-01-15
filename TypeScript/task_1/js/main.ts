fc396c080edcbf6a663806d1ab9eddd15b4cce75
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-15 16:42:22 +0100

    Adding a function interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,3 +14,15 @@ interface Directors extends Teacher {
 }
 
 // On etend l'interface de teacher et on rajoute un attribut
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+};
+
+// console.log(printTeacher("John", "Doe")); -> La commande de test
+
+// On cree une interface pour la fonction printTeacher
